a11b1aa9f5f60e06505aadf2800c983ebdeb63cc
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:17:18 +0200

    Added static import for cleaner code

--- src/main/java/redstonetweaks/mixin/server/BubbleColumnBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/BubbleColumnBlockMixin.java
@@ -1,18 +1,18 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.bubbleColumnDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.block.BubbleColumnBlock;
 
-import redstonetweaks.setting.Settings;
-
 @Mixin(BubbleColumnBlock.class)
 public class BubbleColumnBlockMixin {
 	
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 5))
-	private int getBubbleColumnDelay(int oldDelay) {
-		return (int)Settings.bubbleColumnDelay.get();
+	private int getTickRateWeightedPressurePlateDelay(int oldDelay) {
+		return bubbleColumnDelay.get();
 	}
 }
