58538327839a99c52b45759e7984a2b9630a8322
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 12:54:02 +0200

    Added static imports for cleaner code

--- src/main/java/redstonetweaks/mixin/client/PistonBlockEntityRendererMixin.java
+++ src/main/java/redstonetweaks/mixin/client/PistonBlockEntityRendererMixin.java
@@ -1,12 +1,13 @@
 package redstonetweaks.mixin.client;
 
+import static redstonetweaks.setting.Settings.delayMultiplier;
+import static redstonetweaks.setting.Settings.pistonDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.client.render.block.entity.PistonBlockEntityRenderer;
-
-import redstonetweaks.setting.Settings;
 
 @Mixin(PistonBlockEntityRenderer.class)
 public class PistonBlockEntityRendererMixin {
@@ -18,22 +19,22 @@ public class PistonBlockEntityRendererMixin {
 	
 	@ModifyConstant(method = "render", constant = @Constant(floatValue = 1.0f))
 	private float renderMaxProgress(float maxProgress) {
-		return (float) getPistonDelay();
+		return getPistonDelay();
 	}
 	
 	@ModifyConstant(method = "render", constant = @Constant(floatValue = 4.0f))
 	private float renderShortPistonHead1(float maxrogress) {
-		return (float) (getPistonDelay() / 2);
+		return getPistonDelay() / 2;
 	}
 	
 	@ModifyConstant(method = "render", constant = @Constant(floatValue = 0.5f))
 	private float renderShortPistonHead2(float maxProgress) {
-		return (float) (getPistonDelay() / 2);
+		return getPistonDelay() / 2;
 	}
 	
 	private int getPistonDelay() {
-		int delay = (int)Settings.pistonDelay.get();
-		int delayMultiplier = (int)Settings.delayMultiplier.get();
-		return delay * delayMultiplier;
+		int delay = pistonDelay.get();
+		int multiplier = delayMultiplier.get();
+		return multiplier * delay;
 	}
 }
