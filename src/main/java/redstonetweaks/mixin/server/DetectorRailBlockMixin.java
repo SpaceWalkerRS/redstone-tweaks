2c538051bca330094791b60d4900f989f01037c1
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:19:47 +0200

    Implemented new signal setting and cleaned up code

--- src/main/java/redstonetweaks/mixin/server/DetectorRailBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/DetectorRailBlockMixin.java
@@ -1,18 +1,29 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.detectorRailSignal;
+import static redstonetweaks.setting.Settings.detectorRailDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.block.DetectorRailBlock;
 
-import redstonetweaks.setting.Settings;
-
 @Mixin(DetectorRailBlock.class)
 public class DetectorRailBlockMixin {
 	
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 20))
 	private int getDetectorRailDelay(int oldDelay) {
-		return (int)Settings.detectorRailDelay.get();
+		return detectorRailDelay.get();
+	}
+	
+	@ModifyConstant(method = "getWeakRedstonePower", constant = @Constant(intValue = 15))
+	private int onGetWeakRedstonePower(int oldValue) {
+		return detectorRailSignal.get();
+	}
+	
+	@ModifyConstant(method = "getStrongRedstonePower", constant = @Constant(intValue = 15))
+	private int onGetStrongRedstonePower(int oldValue) {
+		return detectorRailSignal.get();
 	}
 }
