75acff6919ef070cb92de765c8db1a2e797bdd39
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:52:55 +0200

    Cleaned up code

--- src/main/java/redstonetweaks/mixin/server/ServerTickSchedulerMixin.java
+++ src/main/java/redstonetweaks/mixin/server/ServerTickSchedulerMixin.java
@@ -1,27 +1,36 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.delayMultiplier;
+
+import java.util.function.Predicate;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.server.world.ServerTickScheduler;
 import net.minecraft.server.world.ServerWorld;
-
-import redstonetweaks.setting.Settings;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ScheduledTick;
+import net.minecraft.world.TickPriority;;
 
 @Mixin(ServerTickScheduler.class)
-public class ServerTickSchedulerMixin<T> {
+public abstract class ServerTickSchedulerMixin<T> {
 	
 	@Shadow ServerWorld world;
+	@Shadow Predicate<T> invalidObjPredicate;
 	
-	// When creating a new scheduled tick, we modify the time argument.
-	// By subtracting the current world time from the scheduled tick time
-	// we can infer the delay. We then multiply this by the delayMultiplier
-	// and add the current world time again.
-	@ModifyArg(method = "schedule", index = 2, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ScheduledTick;<init>"))
-	public long scheduledTickTimeArg(long oldScheduledTime) {
-		long worldTime = this.world.getTime();
-		return (long)(int)Settings.delayMultiplier.get() * (oldScheduledTime - worldTime) + worldTime;
+	@Shadow abstract void addScheduledTick(ScheduledTick<T> scheduledTick);
+	
+	// Whenever a new tick is scheduled, we need to multiply the delay
+	// by the delay multiplier, then schedule the tick with the new delay.
+	@Inject(method = "schedule", at = @At(value = "HEAD"), cancellable = true)
+	private void onSchedule(BlockPos pos, T object, int delay, TickPriority priority, CallbackInfo ci) {
+		if (!this.invalidObjPredicate.test(object)) {
+			this.addScheduledTick(new ScheduledTick<T>(pos, object, delayMultiplier.get() * delay + this.world.getTime(), priority));
+	   	}
+		ci.cancel();
 	}
 }
