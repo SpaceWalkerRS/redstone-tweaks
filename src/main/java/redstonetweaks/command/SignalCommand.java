a82ccf5b621a2e9a772118d0d96442b2e269a61a
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 12:47:58 +0200

    More consistent variable naming

--- src/main/java/redstonetweaks/command/SignalCommand.java
+++ src/main/java/redstonetweaks/command/SignalCommand.java
@@ -1,6 +1,6 @@
 package redstonetweaks.command;
 
-import java.util.Collection;
+import java.util.ArrayList;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
@@ -21,39 +21,39 @@ public class SignalCommand {
 			return context.hasPermissionLevel(2);
 		});
 		
-		Collection<Setting<?>> blockSignals = Settings.getSettings("block_signal");
+		ArrayList<Setting<?>> settings = Settings.getSettings("signal");
 		
 		builder.then(CommandManager.literal("RESET").executes(context -> {
-			return resetValues(context.getSource(), blockSignals);
+			return resetValues(context.getSource(), settings);
 		}));
 		
-		for (Setting<?> blockSignal : blockSignals) {
-			builder.then(CommandManager.literal(blockSignal.getCommandIdentifier()).executes(context -> {
-				return queryValue(context.getSource(), blockSignal);
-			}).then(blockSignal.argument("value").suggests((context, suggestionsBuilder) -> CommandSource.suggestMatching(new String[]{blockSignal.getDefault().toString()}, suggestionsBuilder)).executes(context -> {
-				return setValue(context, blockSignal);
+		for (Setting<?> setting : settings) {
+			builder.then(CommandManager.literal(setting.getCommandArgumentIdentifier()).executes(context -> {
+				return queryValue(context.getSource(), setting);
+			}).then(setting.argument("value").suggests((context, suggestionsBuilder) -> CommandSource.suggestMatching(setting.getCommandSuggestions(), suggestionsBuilder)).executes(context -> {
+				return setValue(context, setting);
 			})));
 		}
 		
 		dispatcher.register(builder);
 	}
 	
-	private static int resetValues(ServerCommandSource source, Collection<Setting<?>> blockSignals) {
-		for (Setting<?> blockSignal : blockSignals) {
-			blockSignal.reset();
+	private static int resetValues(ServerCommandSource source, ArrayList<Setting<?>> settings) {
+		for (Setting<?> setting : settings) {
+			setting.reset();
 		}
 		source.sendFeedback(new TranslatableText("All signal strengths have been reset to their default values"), false);
 		return 1;
 	}
 	
-	private static int queryValue(ServerCommandSource source, Setting<?> blockSignal) {
-		source.sendFeedback(new TranslatableText("The %s signal strength is currently set to %s", blockSignal.getCommandIdentifier(), blockSignal.get()), false);
+	private static int queryValue(ServerCommandSource source, Setting<?> setting) {
+		source.sendFeedback(new TranslatableText("The %s signal strength is currently set to %s", setting.getCommandArgumentIdentifier(), setting.get()), false);
 		return 1;
 	}
 	
-	private static int setValue(CommandContext<ServerCommandSource> context, Setting<?> blockSignal) throws CommandSyntaxException {
-		blockSignal.setFromArgument(context, "value");
-		context.getSource().sendFeedback(new TranslatableText("The %s signal strength has been set to %s", blockSignal.getCommandIdentifier(), blockSignal.getFromArgument(context, "value")), false);
+	private static int setValue(CommandContext<ServerCommandSource> context, Setting<?> setting) throws CommandSyntaxException {
+		setting.setFromArgument(context, "value");
+		context.getSource().sendFeedback(new TranslatableText("The %s signal strength has been set to %s", setting.getCommandArgumentIdentifier(), setting.getFromArgument(context, "value")), false);
 		return 1;
 	}
 }
