6602f06a7fba549f3362615ec858ada7807e4a0f
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:30:08 +0200

    Removed redundant semi colon

--- src/main/java/redstonetweaks/mixin/server/LeavesBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/LeavesBlockMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import net.minecraft.block.LeavesBlock;;
+import net.minecraft.block.LeavesBlock;
 
 @Mixin(LeavesBlock.class)
 public class LeavesBlockMixin {
