15d94f1db90c177320c3e4abd5da32140ff6300f
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:26:24 +0200

    Added static import for cleaner code

--- src/main/java/redstonetweaks/mixin/server/FallingBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/FallingBlockMixin.java
@@ -1,19 +1,19 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.gravityBlockDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import net.minecraft.block.FallingBlock;
-
-import redstonetweaks.setting.Settings;
+import net.minecraft.block.FallingBlock;;
 
 @Mixin(FallingBlock.class)
 public class FallingBlockMixin {
 	
-	// This is the delay gravity blocks have before they start falling.
+	// Modify the delay gravity blocks have before falling
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 2))
 	private int getGravityBlockDelay(int oldDelay) {
-		return (int)Settings.gravityBlockDelay.get();
+		return gravityBlockDelay.get();
 	}
 }
