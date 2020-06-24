86848b72b0e817a8aaf151e0966225a94d359290
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 14:40:29 +0200

    Fixed dropper delay not working with the compiled mod

--- src/main/java/redstonetweaks/mixin/server/DropperBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/DropperBlockMixin.java
@@ -4,12 +4,18 @@ import static redstonetweaks.setting.Settings.dropperDelay;
 
 import org.spongepowered.asm.mixin.Mixin;
 
+import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.DropperBlock;
 import net.minecraft.world.WorldView;
 
 @Mixin(DropperBlock.class)
-public class DropperBlockMixin {
+public class DropperBlockMixin extends DispenserBlock {
 	
+	protected DropperBlockMixin(Settings settings) {
+		super(settings);
+	}
+	
+	@Override
 	public int getTickRate(WorldView world) {
 		return dropperDelay.get();
 	}
