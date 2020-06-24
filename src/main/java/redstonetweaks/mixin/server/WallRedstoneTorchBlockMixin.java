a342c3881382518aa574618d10dd466584bac60b
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:56:01 +0200

    Cleaned up code
    Added static imports for better readability and changed an @Overwrite into an @Inject

--- src/main/java/redstonetweaks/mixin/server/WallRedstoneTorchBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/WallRedstoneTorchBlockMixin.java
@@ -1,9 +1,16 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.softInversion;
+import static redstonetweaks.setting.Settings.redstoneTorchSignal;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PistonBlock;
@@ -12,32 +19,35 @@ import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-
 import redstonetweaks.helper.PistonHelper;
-import redstonetweaks.setting.Settings;
 
 @Mixin(WallRedstoneTorchBlock.class)
 public class WallRedstoneTorchBlockMixin {
 	
 	@Shadow @Final private static DirectionProperty FACING;
 	
-	@Overwrite
-	public boolean shouldUnpower(World world, BlockPos pos, BlockState state) {
+	@Inject(method = "shouldUnpower", at = @At(value = "HEAD"), cancellable = true)
+	private void shouldUnpower(World world, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir) {
 		Direction attached = state.get(FACING).getOpposite();
 		BlockPos blockPos = pos.offset(attached);
 		
 		// If the pistonsPowerRedstoneTorches setting is enabled,
 		// return true if the torch is attached to a piston that is
 		// receiving redstone power.
-		if ((boolean)Settings.pistonsPowerRedstoneTorches.get()) {
+		if (softInversion.get()) {
 			BlockState blockState = world.getBlockState(blockPos);
 			
 			if (blockState.getBlock() instanceof PistonBlock) {
 				if (PistonHelper.shouldExtend(world, blockPos, blockState.get(PistonBlock.FACING))) {
-					return true;
+					cir.setReturnValue(true);
+					cir.cancel();
 				}
 			}
 		}
-		return world.isEmittingRedstonePower(blockPos, attached);
+	}
+	
+	@ModifyConstant(method = "getWeakRedstonePower", constant = @Constant(intValue = 15))
+	private int onGetWeakRedstonePower(int oldValue) {
+		return redstoneTorchSignal.get();
 	}
 }
