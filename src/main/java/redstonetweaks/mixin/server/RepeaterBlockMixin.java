9ec3fadd4aa9d3b6d706f0614dc18fe1d7a733b2
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:48:10 +0200

    Added static import for cleaner code

--- src/main/java/redstonetweaks/mixin/server/RepeaterBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/RepeaterBlockMixin.java
@@ -1,18 +1,23 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.repeaterDelay;
+import static redstonetweaks.setting.Settings.scaffoldingDelay;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import net.minecraft.block.RepeaterBlock;;
+import net.minecraft.block.ScaffoldingBlock;;
 
-@Mixin(RepeaterBlock.class)
-public class RepeaterBlockMixin {
+@Mixin(ScaffoldingBlock.class)
+public class ScaffoldingBlockMixin {
 	
-	@ModifyConstant(method = "getUpdateDelayInternal", constant = @Constant(intValue = 2))
-	private int getRepeaterDelay(int oldValue) {
-		return repeaterDelay.get();
+	@ModifyConstant(method = "onBlockAdded", constant = @Constant(intValue = 1))
+	private int onBlockAddedGetDelay(int oldValue) {
+		return scaffoldingDelay.get();
+	}
+	
+	@ModifyConstant(method = "getStateForNeighborUpdate", constant = @Constant(intValue = 1))
+	private int getStateForNeighborUpdateGetDelay(int oldValue) {
+		return scaffoldingDelay.get();
 	}
 }
