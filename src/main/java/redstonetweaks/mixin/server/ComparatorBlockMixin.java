111c4e1928b4736ca6713edf988e3f5a69b3fece
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:19:06 +0200

    Added static import for cleaner code

--- src/main/java/redstonetweaks/mixin/server/ComparatorBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/ComparatorBlockMixin.java
@@ -1,18 +1,18 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.comparatorDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import net.minecraft.block.ComparatorBlock;
-
-import redstonetweaks.setting.Settings;
+import net.minecraft.block.ComparatorBlock;;
 
 @Mixin(ComparatorBlock.class)
 public class ComparatorBlockMixin {
 	
 	@ModifyConstant(method = "updatePowered", constant = @Constant(intValue = 2))
-	private int UpdatePoweredDelay(int oldDelay) {
-		return (int)Settings.comparatorDelay.get();
+	private int getComparatorDelay(int oldDelay) {
+		return comparatorDelay.get();
 	}
 }
