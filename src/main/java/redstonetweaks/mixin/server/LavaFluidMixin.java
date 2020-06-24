15eceab1eab8c8da94ae7eb9f30ba11a2c3eaa90
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:28:39 +0200

    Added static imports for cleaner code

--- src/main/java/redstonetweaks/mixin/server/LavaFluidMixin.java
+++ src/main/java/redstonetweaks/mixin/server/LavaFluidMixin.java
@@ -1,23 +1,24 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.lavaDefaultDelay;
+import static redstonetweaks.setting.Settings.lavaNetherDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.fluid.LavaFluid;
 
-import redstonetweaks.setting.Settings;
-
 @Mixin(LavaFluid.class)
 public class LavaFluidMixin {
 	
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 10))
-	private int getLavaNetherDelay(int oldDelay) {
-		return (int)Settings.lavaNetherDelay.get();
+	private int onGetTickRateNetherDelay(int oldValue) {
+		return lavaNetherDelay.get();
 	}
 	
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 30))
-	private int getLavaDefaultDelay(int oldDelay) {
-		return (int)Settings.lavaDefaultDelay.get();
+	private int onGetTickRateDefaultDelay(int oldValue) {
+		return lavaDefaultDelay.get();
 	}
 }
