30b8190f033d297edb4b5e98cf0a797dd2dc7a11
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:55:01 +0200

    Implemented signal strength setting and cleaned up code
    Added static import for better readability

--- src/main/java/redstonetweaks/mixin/server/TripwireHookBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/TripwireHookBlockMixin.java
@@ -1,8 +1,12 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.tripwireHookSignal;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.block.TripwireHookBlock;
 
@@ -13,6 +17,16 @@ public class TripwireHookBlockMixin {
 	
 	@ModifyArg(method = "update", index = 2, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
 	private int modifyGetTickRate(int oldDelay) {
-		return (int)Settings.tripwireHookDelay.get();
+		return Settings.tripwireHookDelay.get();
+	}
+	
+	@ModifyConstant(method = "getWeakRedstonePower", constant = @Constant(intValue = 15))
+	private int onGetWeakRedstonePower(int oldValue) {
+		return tripwireHookSignal.get();
+	}
+	
+	@ModifyConstant(method = "getStrongRedstonePower", constant = @Constant(intValue = 15))
+	private int onGetStrongRedstonePower(int oldValue) {
+		return tripwireHookSignal.get();
 	}
 }
