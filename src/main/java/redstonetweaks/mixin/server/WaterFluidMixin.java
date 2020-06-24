0a8306c59107efbcaf2dd2cd2f1d8e9f2acf87d8
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:56:41 +0200

    Added static import for cleaner code

--- src/main/java/redstonetweaks/mixin/server/WaterFluidMixin.java
+++ src/main/java/redstonetweaks/mixin/server/WaterFluidMixin.java
@@ -1,18 +1,18 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.waterDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.fluid.WaterFluid;
 
-import redstonetweaks.setting.Settings;
-
 @Mixin(WaterFluid.class)
 public class WaterFluidMixin {
 	
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 5))
-	private int getTickRateDelay(int oldDelay) {
-		return (int)Settings.waterDelay.get();
+	private int getWaterDelay(int oldDelay) {
+		return waterDelay.get();
 	}
 }
