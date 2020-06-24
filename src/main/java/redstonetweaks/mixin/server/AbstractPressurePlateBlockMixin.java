1bd43120f7ed50bb83b16b6b319bb75be18b6816
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:15:10 +0200

    Cleaned up code and implemented new settings

--- src/main/java/redstonetweaks/mixin/server/AbstractPressurePlateBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/AbstractPressurePlateBlockMixin.java
@@ -1,49 +1,66 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.heavyWeightedPressurePlateOnDelay;
+import static redstonetweaks.setting.Settings.lightWeightedPressurePlateOnDelay;
+import static redstonetweaks.setting.Settings.stonePressurePlateDelay;
+import static redstonetweaks.setting.Settings.stonePressurePlateOnDelay;
+import static redstonetweaks.setting.Settings.woodenPressurePlateDelay;
+import static redstonetweaks.setting.Settings.woodenPressurePlateOnDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.block.AbstractPressurePlateBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
 import net.minecraft.block.PressurePlateBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.TickPriority;
+import net.minecraft.world.TickScheduler;
 import net.minecraft.world.World;
-
-import redstonetweaks.setting.Settings;
 
 @Mixin(AbstractPressurePlateBlock.class)
 public abstract class AbstractPressurePlateBlockMixin {
 	
 	@Shadow protected abstract void updatePlateState(World world, BlockPos pos, BlockState blockState, int rsOut);
-	
-	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 20))
-	private int getPressurePlateDelay(int oldDelay) {
-		return (int)Settings.pressurePlateDelay.get();
-	}
+	@Shadow protected abstract int getRedstoneOutput(BlockState state);
 	
 	// When the pressure plate is ticked, it should call updatePlateState
 	// regardless of its current redstone output, in case the plate
 	// has activation delay and the plate is ticked to power on. 
-	// To do that the call to getRedstoneOutput is redirected and
-	// a value of 1 is returned.
 	@Redirect(method = "scheduledTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractPressurePlateBlock;getRedstoneOutput(Lnet/minecraft/block/BlockState;)I"))
-	public int scheduledTickRedirectGetRedstoneOutput(AbstractPressurePlateBlock pressurePlate, BlockState state) {
+	private int onScheduledTickRedirectGetRedstoneOutput(AbstractPressurePlateBlock pressurePlate, BlockState state) {
 		return 1;
 	}
 	
 	@Redirect(method = "onEntityCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractPressurePlateBlock;updatePlateState(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)V"))
 	public void onEntityCollisionRedirectUpdatePlateState(AbstractPressurePlateBlock pressurePlate, World world, BlockPos pos, BlockState state, int i) {
-		int delay = (Block)(Object) this instanceof PressurePlateBlock ? (int)Settings.pressurePlateOnDelay.get() : (int)Settings.weightedPressurePlateOnDelay.get();
+		int delay;
+		if (state.getBlock() instanceof PressurePlateBlock) {
+			delay = pressurePlate.getMaterial(state) == Material.WOOD ? woodenPressurePlateOnDelay.get() : stonePressurePlateOnDelay.get();
+		} else {
+			delay = state.getBlock() == Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE ? heavyWeightedPressurePlateOnDelay.get() : lightWeightedPressurePlateOnDelay.get();
+		}
 		if (delay > 0) {
-			world.getBlockTickScheduler().schedule(pos, (Block)(Object) this, delay, TickPriority.EXTREMELY_HIGH);
+			world.getBlockTickScheduler().schedule(pos, state.getBlock(), delay, TickPriority.EXTREMELY_HIGH);
 		} else {
 			this.updatePlateState(world, pos, state, i);
 		}
+	}
+	
+	// We cannot just redirect the getTickRate(WorldView) call
+	// because the delay depends on what type of pressure plate it is.
+	@Redirect(method = "updatePlateState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void updatePlateStateRedirectScheduleTick(TickScheduler<?> tickScheduler, BlockPos pos, T Object, int oldDelay, World world, BlockPos blockPos, BlockState state) {
+		int delay;
+		if (state.getBlock() instanceof PressurePlateBlock) {
+			delay = state.getBlock().getMaterial(state) == Material.WOOD ? woodenPressurePlateDelay.get() : stonePressurePlateDelay.get();
+		} else {
+			delay = state.getBlock().getTickRate(world);
+		}
+		world.getBlockTickScheduler().schedule(pos, state.getBlock(), delay);
 	}
 }
