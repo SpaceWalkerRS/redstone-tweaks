9ac5dbf9f92af79dfe71e339657031aa0719ee89
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:23:10 +0200

    Cleaned up code
    Added static imports for better readability and removed the dropper delay from the ModifyConstant for getTickRate. The dropper delay is now handled in its own mixin class.

--- src/main/java/redstonetweaks/mixin/server/DispenserBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/DispenserBlockMixin.java
@@ -1,5 +1,14 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.dispenserDelay;
+import static redstonetweaks.setting.Settings.quasiConnectivityDown;
+import static redstonetweaks.setting.Settings.quasiConnectivityEast;
+import static redstonetweaks.setting.Settings.quasiConnectivityNorth;
+import static redstonetweaks.setting.Settings.quasiConnectivitySouth;
+import static redstonetweaks.setting.Settings.quasiConnectivityUp;
+import static redstonetweaks.setting.Settings.quasiConnectivityWest;
+import static redstonetweaks.setting.Settings.randomizeQuasiConnectivity;
+
 import java.util.Random;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,23 +17,16 @@ import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import redstonetweaks.setting.Settings;
 
 @Mixin(DispenserBlock.class)
 public class DispenserBlockMixin {
 	
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 4))
-	private int getDelay(int oldDelay) {
-		if ((Block)(Object)this == Blocks.DISPENSER) {
-			return (int)Settings.dispenserDelay.get();
-		}
-		return (int)Settings.dropperDelay.get();
+	private int getDispenserDelay(int oldDelay) {
+		return dispenserDelay.get();
 	}
 	
 	// Both the dispenser and the dropper are affected by Quasi-Connectivity
@@ -33,22 +35,23 @@ public class DispenserBlockMixin {
 	@Redirect(method = "neighborUpdate", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/world/World;isReceivingRedstonePower(Lnet/minecraft/util/math/BlockPos;)Z"))
 	private boolean neighborUpdateRedirectIsReceivingRedstonePower(World world, BlockPos posUp) {
 		BlockPos pos = posUp.down();
-		if ((boolean)Settings.randomizeQuasiConnectivity.get() ? (new Random()).nextBoolean() : (boolean)Settings.quasiConnectivityDown.get()) {
+		boolean randQC = randomizeQuasiConnectivity.get();
+		if (randQC ? (new Random()).nextBoolean() : quasiConnectivityDown.get()) {
 			return world.isReceivingRedstonePower(pos.down());
 		}
-		if ((boolean)Settings.randomizeQuasiConnectivity.get() ? (new Random()).nextBoolean() : (boolean)Settings.quasiConnectivityEast.get()) {
+		if (randQC ? (new Random()).nextBoolean() : quasiConnectivityEast.get()) {
 			return world.isReceivingRedstonePower(pos.east());
 		}
-		if ((boolean)Settings.randomizeQuasiConnectivity.get() ? (new Random()).nextBoolean() : (boolean)Settings.quasiConnectivityNorth.get()) {
+		if (randQC ? (new Random()).nextBoolean() : quasiConnectivityNorth.get()) {
 			return world.isReceivingRedstonePower(pos.north());
 		}
-		if ((boolean)Settings.randomizeQuasiConnectivity.get() ? (new Random()).nextBoolean() : (boolean)Settings.quasiConnectivitySouth.get()) {
+		if (randQC ? (new Random()).nextBoolean() : quasiConnectivitySouth.get()) {
 			return world.isReceivingRedstonePower(pos.south());
 		}
-		if ((boolean)Settings.randomizeQuasiConnectivity.get() ? (new Random()).nextBoolean() : (boolean)Settings.quasiConnectivityUp.get()) {
+		if (randQC ? (new Random()).nextBoolean() : quasiConnectivityUp.get()) {
 			return world.isReceivingRedstonePower(pos.up());
 		}
-		if ((boolean)Settings.randomizeQuasiConnectivity.get() ? (new Random()).nextBoolean() : (boolean)Settings.quasiConnectivityWest.get()) {
+		if (randQC ? (new Random()).nextBoolean() : quasiConnectivityWest.get()) {
 			return world.isReceivingRedstonePower(pos.west());
 		}
 		return false;
