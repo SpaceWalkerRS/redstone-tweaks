70430883172652a878308eb3fedc55a2badf9849
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:53:44 +0200

    Added static import for cleaner code

--- src/main/java/redstonetweaks/mixin/server/TntEntityMixin.java
+++ src/main/java/redstonetweaks/mixin/server/TntEntityMixin.java
@@ -1,28 +1,28 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.tntDelay;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import net.minecraft.entity.TntEntity;
-
-import redstonetweaks.setting.Settings;
+import net.minecraft.entity.TntEntity;;
 
 @Mixin(TntEntity.class)
 public class TntEntityMixin {
 	
 	@ModifyConstant(method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V", constant = @Constant(intValue = 80))
-	private int init1FuseTime(int oldFuseTime) {
-		return (int)Settings.tntDelay.get();
+	private int init2FuseTime(int oldFuseTime) {
+		return tntDelay.get();
 	}
 	
 	@ModifyConstant(method = "<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/entity/LivingEntity;)V", constant = @Constant(intValue = 80))
-	private int init2FuseTime(int oldFuseTime) {
-		return (int)Settings.tntDelay.get();
+	private int init5FuseTime(int oldFuseTime) {
+		return tntDelay.get();
 	}
 	
 	@ModifyConstant(method = "initDataTracker", constant = @Constant(intValue = 80))
 	private int initDataTrackerFuseTime(int oldFuseTime) {
-		return (int)Settings.tntDelay.get();
+		return tntDelay.get();
 	}
 }
