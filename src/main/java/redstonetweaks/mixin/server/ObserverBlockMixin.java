8bb2a086bc304728ed3cb95400018288c686b302
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-06-24 13:33:46 +0200

    Implemented new signal strength setting and cleaned up code
    Added static import for better readability, removed the @Overwrite and split it into a @Redirect and an @Inject.

--- src/main/java/redstonetweaks/mixin/server/ObserverBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/ObserverBlockMixin.java
@@ -1,22 +1,36 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.Settings.MC136566;
+import static redstonetweaks.setting.Settings.MC137127;
+import static redstonetweaks.setting.Settings.MC189954;
+import static redstonetweaks.setting.Settings.observerDelay;
+import static redstonetweaks.setting.Settings.observerSignal;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.FacingBlock;
 import net.minecraft.block.ObserverBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.TickPriority;
-
-import redstonetweaks.setting.Settings;
+import net.minecraft.world.TickScheduler;
 
 @Mixin(ObserverBlock.class)
-public class ObserverBlockMixin {
+public class ObserverBlockMixin<T> extends FacingBlock {
 	
+	protected ObserverBlockMixin(Settings settings) {
+		super(settings);
+	}
+
 	// To fix MC-136566 (https://bugs.mojang.com/browse/MC-136566)
 	// and MC-137127 (https://bugs.mojang.com/browse/MC-137127)
 	// we change the flags argument given to the setBlockState call.
@@ -26,10 +40,10 @@ public class ObserverBlockMixin {
 	@ModifyConstant(method = "onBlockAdded", constant = @Constant(intValue = 18))
 	private int onBlockAddedFlags(int oldFlags) {
 		int flags = oldFlags;
-		if ((boolean)Settings.MC136566.get()) {
+		if (MC136566.get()) {
 			flags += 1;
 		}
-		if ((boolean)Settings.MC137127.get()) {
+		if (MC137127.get()) {
 			flags += 48;
 		}
 		return flags;
@@ -37,7 +51,7 @@ public class ObserverBlockMixin {
 	
 	@ModifyConstant(method = "scheduledTick", constant = @Constant(intValue = 2, ordinal = 2))
 	private int scheduledTickObserverDelay(int oldDelay) {
-		return (int)Settings.observerDelay.get();
+		return observerDelay.get();
 	}
 	
 	// The scheduleTick method is only called from 
@@ -51,21 +65,31 @@ public class ObserverBlockMixin {
 	// The second part of this bug fix is to change the tick priority
 	// of the observer if it detects an observer facing away from it.
 	// That way 4 tick observer clocks still work if the bug fix is enabled.
-	@Overwrite
-	private void scheduleTick(IWorld world, BlockPos pos) {
-		if ((boolean)Settings.MC189954.get()) {
-			if (!world.isClient() && !world.getBlockTickScheduler().isTicking(pos, (ObserverBlock)(Object)this)) {
-				BlockState state = world.getBlockState(pos);
-				BlockPos pos2 = pos.offset(state.get(ObserverBlock.FACING));
-				BlockState state2 = world.getBlockState(pos2);
-				if (state2.getBlock() == Blocks.OBSERVER && state2.get(ObserverBlock.FACING) != state.get(ObserverBlock.FACING)) {
-					world.getBlockTickScheduler().schedule(pos, (ObserverBlock)(Object)this, (int)Settings.observerDelay.get(), TickPriority.LOW);
-				} else {
-					world.getBlockTickScheduler().schedule(pos, (ObserverBlock)(Object)this, (int)Settings.observerDelay.get());
-				}
-			}
-		} else if (!world.isClient() && !world.getBlockTickScheduler().isScheduled(pos, (ObserverBlock)(Object)this)) {
-			world.getBlockTickScheduler().schedule(pos, (ObserverBlock)(Object)this, (int)Settings.observerDelay.get());
+	@Redirect(method = "scheduleTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;isScheduled(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;)Z"))
+	private boolean onScheduleTickRedirectIsScheduled(TickScheduler<T> tickScheduler, BlockPos pos, T object) {
+		if (MC189954.get()) {
+			return tickScheduler.isTicking(pos, object);
+		} else {
+			return tickScheduler.isScheduled(pos, object);
 		}
+	}
+	
+	@Inject(method = "scheduleTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V", shift = Shift.BEFORE))
+	private void onScheduleTickInjectBeforeSchedule(IWorld world, BlockPos pos, CallbackInfo ci) {
+		BlockState state = world.getBlockState(pos);
+		TickPriority tickPriority = TickPriority.NORMAL;
+		if (MC189954.get()) {
+			BlockPos pos2 = pos.offset(state.get(FACING));
+			BlockState state2 = world.getBlockState(pos2);
+			if (state2.getBlock() == Blocks.OBSERVER && state2.get(FACING) != state.get(FACING)) {
+				tickPriority = TickPriority.LOW;
+			}
+		}
+		world.getBlockTickScheduler().schedule(pos, state.getBlock(), observerDelay.get(), tickPriority);
+	}
+	
+	@ModifyConstant(method = "getWeakRedstonePower", constant = @Constant(intValue = 15))
+	private int onGetWeakRedstonePower(int oldValue) {
+		return observerSignal.get();
 	}
 }
