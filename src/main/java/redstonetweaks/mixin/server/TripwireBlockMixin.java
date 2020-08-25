f2434c33a12976cafa23482da8db4139d5592057
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 16:21:13 +0200

    Update TripwireBlockMixin.java

--- src/main/java/redstonetweaks/mixin/server/TripwireBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/TripwireBlockMixin.java
@@ -1,18 +1,20 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.tripwireDelay;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.block.TripwireBlock;;
+import net.minecraft.block.TripwireBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.TickScheduler;;
 
 @Mixin(TripwireBlock.class)
 public class TripwireBlockMixin {
 	
-	@ModifyArg(method = "updatePowered", index = 2, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
-	private int modifyGetTickRate(int oldDelay) {
-		return tripwireDelay.get();
+	@Redirect(method = "updatePowered", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onUpdatePoweredRedirectSchedule(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, TRIPWIRE.get(DELAY), TRIPWIRE.get(TICK_PRIORITY));
 	}
 }
