2bd6c6298a114ea74a1141bd8b85bad0b6991f2f
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 16:09:51 +0200

    Update PressurePlateBlockMixin.java

--- src/main/java/redstonetweaks/mixin/server/PressurePlateBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/PressurePlateBlockMixin.java
@@ -1,48 +1,25 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.stonePressurePlateSignal;
-import static redstonetweaks.setting.Settings.woodenPressurePlateSignal;
+import static redstonetweaks.setting.SettingsManager.*;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.block.AbstractPressurePlateBlock;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Material;
 import net.minecraft.block.PressurePlateBlock;
-import net.minecraft.entity.EntityContext;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.BlockView;
+import net.minecraft.state.property.Properties;
+
+import redstonetweaks.helper.PressurePlateHelper;
 
 @Mixin(PressurePlateBlock.class)
-public abstract class PressurePlateBlockMixin extends AbstractPressurePlateBlock {
+public class PressurePlateBlockMixin implements PressurePlateHelper {
 	
-	@Shadow @Final public static BooleanProperty POWERED;
-	
-	protected PressurePlateBlockMixin(Settings settings) {
-		super(settings);
-	}
-	
-	@Inject(method = "getRedstoneOutput", at = @At(value = "HEAD"), cancellable = true)
-	private void getRedstoneOutput(BlockState state, CallbackInfoReturnable<Integer> cir) {
-		int power = state.getBlock().getMaterial(state) == Material.WOOD ? woodenPressurePlateSignal.get() : stonePressurePlateSignal.get();
-		cir.setReturnValue(state.get(POWERED) ? power : 0);
-		cir.cancel();
-	}
-	
-	// We need to override this function because when the blocks
-	// are initialized, this method is called, and in vanilla it
-	// calls the getRedstoneOutput(BlockState), but the Redstone
-	// Tweaks settings have not yet been initialized, which would
-	// cause a crash.
 	@Override
-	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext context) {
-		return state.get(POWERED) ? PRESSED_SHAPE : DEFAULT_SHAPE;
+	public int getWeakPower(BlockState state) {
+		return state.get(Properties.POWERED) ? BLOCK_TO_SETTINGS_PACK.get(state.getBlock()).get(WEAK_POWER) : 0;
+	}
+
+	@Override
+	public int getStrongPower(BlockState state) {
+		return state.get(Properties.POWERED) ? BLOCK_TO_SETTINGS_PACK.get(state.getBlock()).get(STRONG_POWER) : 0;
 	}
 }
