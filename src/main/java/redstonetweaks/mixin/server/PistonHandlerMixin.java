c04afaa7bdb7ca0ca49f09b55f2f4f4a5dd7cee0
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 15:58:01 +0200

    Update PistonHandlerMixin.java

--- src/main/java/redstonetweaks/mixin/server/PistonHandlerMixin.java
+++ src/main/java/redstonetweaks/mixin/server/PistonHandlerMixin.java
@@ -1,18 +1,55 @@
 package redstonetweaks.mixin.server;
 
+import static redstonetweaks.setting.SettingsManager.*;
+
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.PistonBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.PistonBlockEntity;
 import net.minecraft.block.piston.PistonHandler;
-
-import static redstonetweaks.setting.Settings.pushLimit;;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 
 @Mixin(PistonHandler.class)
 public class PistonHandlerMixin {
 	
+	@Shadow @Final private boolean retracted;
+	@Shadow @Final private BlockPos posTo;
+	
+	private boolean sticky;
+	
+	@Inject(method = "<init>", at = @At(value = "RETURN"))
+	private void onInitInjectAtReturn(World world, BlockPos pos, Direction dir, boolean retracted, CallbackInfo ci) {
+		BlockState state = world.getBlockState(pos);
+		if (state.getBlock() instanceof PistonBlock) {
+			sticky = state.isOf(Blocks.STICKY_PISTON);
+		} else if (state.isOf(Blocks.MOVING_PISTON)) {
+			BlockEntity blockEntity = world.getBlockEntity(pos);
+			if (blockEntity instanceof PistonBlockEntity) {
+				PistonBlockEntity pistonBlockEntity = (PistonBlockEntity)blockEntity;
+				if (pistonBlockEntity.isSource()) {
+					BlockState pushedState = pistonBlockEntity.getPushedBlock();
+					if (pushedState.getBlock() instanceof PistonBlock) {
+						sticky = pushedState.isOf(Blocks.STICKY_PISTON);
+					}
+				}
+			}
+		}
+	}
+	
 	@ModifyConstant(method = "tryMove", constant = @Constant(intValue = 12))
 	private int pushLimit(int oldPushLimit) {
-		return pushLimit.get();
+		return sticky ? STICKY_PISTON.get(PUSH_LIMIT) : NORMAL_PISTON.get(PUSH_LIMIT);
 	}
 }
