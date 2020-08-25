f0408c9d18ddbed5d5dac25b7c6d12122a637641
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 15:54:09 +0200

    Update LeavesBlockMixin.java

--- src/main/java/redstonetweaks/mixin/server/LeavesBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/LeavesBlockMixin.java
@@ -1,18 +1,20 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.leavesDelay;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.block.LeavesBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.TickScheduler;
 
 @Mixin(LeavesBlock.class)
 public class LeavesBlockMixin {
 	
-	@ModifyConstant(method = "getStateForNeighborUpdate", constant = @Constant(intValue = 1, ordinal = 2))
-	private int getLeavesDelay(int oldValue) {
-		return leavesDelay.get();
+	@Redirect(method = "getStateForNeighborUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onGetStateForNeighborUpdateRedirectSchedule(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, LEAVES.get(DELAY), LEAVES.get(TICK_PRIORITY));
 	}
 }
