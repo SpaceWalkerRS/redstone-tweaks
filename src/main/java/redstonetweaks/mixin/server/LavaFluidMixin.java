0a12c5e17d0f966a09b0d1090b0ec2e060add64e
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 15:53:42 +0200

    Update LavaFluidMixin.java

--- src/main/java/redstonetweaks/mixin/server/LavaFluidMixin.java
+++ src/main/java/redstonetweaks/mixin/server/LavaFluidMixin.java
@@ -1,24 +1,30 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.lavaDefaultDelay;
-import static redstonetweaks.setting.Settings.lavaNetherDelay;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.fluid.LavaFluid;
+import net.minecraft.world.TickPriority;
+import redstonetweaks.helper.FluidHelper;
 
 @Mixin(LavaFluid.class)
-public class LavaFluidMixin {
+public class LavaFluidMixin implements FluidHelper {
 	
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 10))
 	private int onGetTickRateNetherDelay(int oldValue) {
-		return lavaNetherDelay.get();
+		return LAVA.get(NETHER_DELAY);
 	}
 	
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 30))
 	private int onGetTickRateDefaultDelay(int oldValue) {
-		return lavaDefaultDelay.get();
+		return LAVA.get(DELAY);
+	}
+	
+	@Override
+	public TickPriority getTickPriority() {
+		return LAVA.get(TICK_PRIORITY);
 	}
 }
