e141346b6062d4ce78155c5ec8dfd481a541793e
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 15:55:35 +0200

    Update ObserverBlockMixin.java

--- src/main/java/redstonetweaks/mixin/server/ObserverBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/ObserverBlockMixin.java
@@ -1,12 +1,11 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.MC136566;
-import static redstonetweaks.setting.Settings.MC137127;
-import static redstonetweaks.setting.Settings.MC189954;
-import static redstonetweaks.setting.Settings.observerDelay;
-import static redstonetweaks.setting.Settings.observerSignal;
+import static redstonetweaks.setting.SettingsManager.*;
+
+import java.util.Random;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -14,82 +13,88 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.FacingBlock;
 import net.minecraft.block.ObserverBlock;
+import net.minecraft.server.world.ServerTickScheduler;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.TickPriority;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.TickScheduler;
+import net.minecraft.world.World;
+
+import redstonetweaks.helper.AbstractBlockHelper;
+import redstonetweaks.helper.ServerWorldHelper;
+import redstonetweaks.helper.WorldHelper;
+import redstonetweaks.world.server.UnfinishedEvent.Source;
 
 @Mixin(ObserverBlock.class)
-public class ObserverBlockMixin<T> extends FacingBlock {
+public abstract class ObserverBlockMixin implements AbstractBlockHelper {
 	
-	protected ObserverBlockMixin(Settings settings) {
-		super(settings);
-	}
-
-	// To fix MC-136566 (https://bugs.mojang.com/browse/MC-136566)
-	// and MC-137127 (https://bugs.mojang.com/browse/MC-137127)
-	// we change the flags argument given to the setBlockState call.
-	// Adding 1 to the flags makes sure neighboring blocks are updated,
-	// fixing MC-136566. Adding 48 to the flags makes sure neighboring
-	// observers are updated, fixing MC-137127.
-	@ModifyConstant(method = "onBlockAdded", constant = @Constant(intValue = 18))
-	private int onBlockAddedFlags(int oldFlags) {
-		int flags = oldFlags;
-		if (MC136566.get()) {
-			flags += 1;
-		}
-		if (MC137127.get()) {
-			flags += 48;
-		}
-		return flags;
-	}
+	@Shadow protected abstract void updateNeighbors(World world, BlockPos pos, BlockState state);
 	
-	@ModifyConstant(method = "scheduledTick", constant = @Constant(intValue = 2, ordinal = 2))
-	private int scheduledTickObserverDelay(int oldDelay) {
-		return observerDelay.get();
-	}
-	
-	// The scheduleTick method is only called from 
-	// inside the getStateForNeighborUpdate method.
-	// If the bug fix for MC-189954 (https://bugs.mojang.com/browse/MC-189954)
-	// is enabled, we call the isTicking method rather than
-	// the isScheduled method.
-	// The isScheduled method will return false if the observer
-	// is scheduled to tick at the current world time, while
-	// the isTicking method will return true.
-	// The second part of this bug fix is to change the tick priority
-	// of the observer if it detects an observer facing away from it.
-	// That way 4 tick observer clocks still work if the bug fix is enabled.
-	@Redirect(method = "scheduleTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;isScheduled(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;)Z"))
-	private boolean onScheduleTickRedirectIsScheduled(TickScheduler<T> tickScheduler, BlockPos pos, T object) {
-		if (MC189954.get()) {
-			return tickScheduler.isTicking(pos, object);
-		} else {
-			return tickScheduler.isScheduled(pos, object);
+	@Inject(method = "scheduledTick", cancellable = true, at = @At(value = "INVOKE", shift = Shift.AFTER, target = "Lnet/minecraft/server/world/ServerWorld;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+	private void onScheduledTickInjectAfterSetBlockState(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+		if (((WorldHelper)world).shouldSeparateUpdates()) {
+			((ServerWorldHelper)world).getUnfinishedEventScheduler().schedule(Source.BLOCK, state, pos, 0);
+			ci.cancel();
 		}
 	}
 	
-	@Inject(method = "scheduleTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V", shift = Shift.BEFORE))
-	private void onScheduleTickInjectBeforeSchedule(IWorld world, BlockPos pos, CallbackInfo ci) {
-		BlockState state = world.getBlockState(pos);
-		TickPriority tickPriority = TickPriority.NORMAL;
-		if (MC189954.get()) {
-			BlockPos pos2 = pos.offset(state.get(FACING));
-			BlockState state2 = world.getBlockState(pos2);
-			if (state2.getBlock() == Blocks.OBSERVER && state2.get(FACING) != state.get(FACING)) {
-				tickPriority = TickPriority.LOW;
-			}
+	@Redirect(method = "scheduledTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerTickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onScheduledTickRedirectSchedule(ServerTickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, OBSERVER.get(FALLING_DELAY), OBSERVER.get(FALLING_TICK_PRIORITY));
+	}
+	
+	@Redirect(method = "scheduleTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onScheduleTickInjectBeforeSchedule(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		if (!OBSERVER.get(DISABLE)) {
+			tickScheduler.schedule(pos, object, OBSERVER.get(RISING_DELAY), OBSERVER.get(RISING_TICK_PRIORITY));
 		}
-		world.getBlockTickScheduler().schedule(pos, state.getBlock(), observerDelay.get(), tickPriority);
+	}
+	
+	@Inject(method = "getStrongRedstonePower", at = @At(value = "HEAD"), cancellable = true)
+	private void onGetStrongRedstonePowerRedirectGetWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction, CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(state.get(Properties.POWERED) && state.get(Properties.FACING) == direction ? OBSERVER.get(STRONG_POWER) : 0);
+		cir.cancel();
 	}
 	
 	@ModifyConstant(method = "getWeakRedstonePower", constant = @Constant(intValue = 15))
 	private int onGetWeakRedstonePower(int oldValue) {
-		return observerSignal.get();
+		return OBSERVER.get(WEAK_POWER);
+	}
+	
+	// To fix MC-136566 (https://bugs.mojang.com/browse/MC-136566)
+	// and MC-137127 (https://bugs.mojang.com/browse/MC-137127)
+	// we change the flags argument given to the setBlockState call.
+	// Enabling the 1 flag makes sure neighboring blocks are updated,
+	// fixing MC-136566. Disabling the 16 flag makes sure neighboring
+	// observers are updated, fixing MC-137127.
+	@ModifyConstant(method = "onBlockAdded", constant = @Constant(intValue = 18))
+	private int onBlockAddedFlags(int oldFlags) {
+		int flags = oldFlags;
+		if (BUG_FIXES.get(MC136566)) {
+			flags |= 1;
+		}
+		if (BUG_FIXES.get(MC137127)) {
+			flags &= ~16;
+		}
+		return flags;
+	}
+
+	@Override
+	public boolean continueEvent(World world, BlockState state, BlockPos pos, int type) {
+		if (type == 0) {
+			if (state.get(Properties.POWERED)) {
+				world.getBlockTickScheduler().schedule(pos, state.getBlock(), OBSERVER.get(FALLING_DELAY), OBSERVER.get(FALLING_TICK_PRIORITY));
+			}
+			
+			updateNeighbors(world, pos, state);
+		}
+		
+		return false;
 	}
 }
