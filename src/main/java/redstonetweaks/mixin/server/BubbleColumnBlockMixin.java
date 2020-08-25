1e1b9c69f37de253720e937060e185bd4840fb79
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 15:49:36 +0200

    Update BubbleColumnBlockMixin.java

--- src/main/java/redstonetweaks/mixin/server/BubbleColumnBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/BubbleColumnBlockMixin.java
@@ -1,18 +1,25 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.bubbleColumnDelay;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.block.BubbleColumnBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.TickScheduler;
 
 @Mixin(BubbleColumnBlock.class)
 public class BubbleColumnBlockMixin {
 	
-	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 5))
-	private int getTickRateWeightedPressurePlateDelay(int oldDelay) {
-		return bubbleColumnDelay.get();
+	@Redirect(method = "getStateForNeighborUpdate", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onGetStateForNeighborUpdateRedirectSchedule0(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, BUBBLE_COLUMN.get(DELAY), BUBBLE_COLUMN.get(TICK_PRIORITY));
+	}
+	
+	@Redirect(method = "getStateForNeighborUpdate", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onGetStateForNeighborUpdateRedirectSchedule1(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, oldDelay, WATER.get(TICK_PRIORITY));
 	}
 }
