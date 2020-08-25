0aedfe33d786730c5b94595553cda505d580a44e
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 16:21:39 +0200

    Update TripwireHookBlockMixin.java

--- src/main/java/redstonetweaks/mixin/server/TripwireHookBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/TripwireHookBlockMixin.java
@@ -1,32 +1,32 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.tripwireHookSignal;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.block.TripwireHookBlock;
-
-import redstonetweaks.setting.Settings;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.TickScheduler;
 
 @Mixin(TripwireHookBlock.class)
 public class TripwireHookBlockMixin {
 	
-	@ModifyArg(method = "update", index = 2, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
-	private int modifyGetTickRate(int oldDelay) {
-		return Settings.tripwireHookDelay.get();
+	@Redirect(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onUpdateRedirectSchedule(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, TRIPWIRE_HOOK.get(DELAY), TRIPWIRE_HOOK.get(TICK_PRIORITY));
 	}
 	
 	@ModifyConstant(method = "getWeakRedstonePower", constant = @Constant(intValue = 15))
 	private int onGetWeakRedstonePower(int oldValue) {
-		return tripwireHookSignal.get();
+		return TRIPWIRE_HOOK.get(WEAK_POWER);
 	}
 	
 	@ModifyConstant(method = "getStrongRedstonePower", constant = @Constant(intValue = 15))
 	private int onGetStrongRedstonePower(int oldValue) {
-		return tripwireHookSignal.get();
+		return TRIPWIRE_HOOK.get(STRONG_POWER);
 	}
 }
