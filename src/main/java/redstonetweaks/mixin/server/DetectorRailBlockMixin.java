ecf2e594ce0e429e31f0648bbcbe09607290ed07
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 15:51:01 +0200

    Update DetectorRailBlockMixin.java

--- src/main/java/redstonetweaks/mixin/server/DetectorRailBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/DetectorRailBlockMixin.java
@@ -1,29 +1,32 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.detectorRailSignal;
-import static redstonetweaks.setting.Settings.detectorRailDelay;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.block.DetectorRailBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.TickScheduler;
 
 @Mixin(DetectorRailBlock.class)
 public class DetectorRailBlockMixin {
 	
-	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 20))
-	private int getDetectorRailDelay(int oldDelay) {
-		return detectorRailDelay.get();
-	}
-	
 	@ModifyConstant(method = "getWeakRedstonePower", constant = @Constant(intValue = 15))
 	private int onGetWeakRedstonePower(int oldValue) {
-		return detectorRailSignal.get();
+		return DETECTOR_RAIL.get(WEAK_POWER);
 	}
 	
 	@ModifyConstant(method = "getStrongRedstonePower", constant = @Constant(intValue = 15))
 	private int onGetStrongRedstonePower(int oldValue) {
-		return detectorRailSignal.get();
+		return DETECTOR_RAIL.get(STRONG_POWER);
+	}
+	
+	@Redirect(method = "updatePoweredStatus", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onUpdatePoweredStatusRedirectSchedule(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, DETECTOR_RAIL.get(DELAY), DETECTOR_RAIL.get(TICK_PRIORITY));
 	}
 }
