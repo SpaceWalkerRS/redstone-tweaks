038edb0c1d5fb56410a76a0da222fb7fd35481ab
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 15:34:11 +0200

    Update PistonBlockEntityRendererMixin.java

--- src/main/java/redstonetweaks/mixin/client/PistonBlockEntityRendererMixin.java
+++ src/main/java/redstonetweaks/mixin/client/PistonBlockEntityRendererMixin.java
@@ -1,26 +1,40 @@
 package redstonetweaks.mixin.client;
 
-import static redstonetweaks.setting.Settings.delayMultiplier;
-import static redstonetweaks.setting.Settings.pistonDelay;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.block.entity.PistonBlockEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.PistonBlockEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+
+import redstonetweaks.helper.PistonBlockEntityHelper;
+import redstonetweaks.setting.IntegerProperty;
+import redstonetweaks.setting.Setting;
+import redstonetweaks.setting.SettingsPack;
 
 @Mixin(PistonBlockEntityRenderer.class)
 public class PistonBlockEntityRendererMixin {
 	
-	// To change the speed of pistons, we allow the maximum progress
+	private boolean sticky;
+	private boolean extending;
+	
+	@Inject(method = "render", at = @At(value = "HEAD"))
+	private void atRenderInjectAtHead(PistonBlockEntity pistonBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, CallbackInfo ci) {
+		sticky = ((PistonBlockEntityHelper)pistonBlockEntity).isMovedByStickyPiston();
+		extending = pistonBlockEntity.isExtending();
+	}
+	
+	// To change the speed of pistons, we allow the progress
 	// of the piston block entity to increment beyond 1.
 	// That means we need to change the values against which the
 	// progress is checked in several places.
-	
-	@ModifyConstant(method = "render", constant = @Constant(floatValue = 1.0f))
-	private float renderMaxProgress(float maxProgress) {
-		return getPistonDelay();
-	}
 	
 	@ModifyConstant(method = "render", constant = @Constant(floatValue = 4.0f))
 	private float renderShortPistonHead1(float maxrogress) {
@@ -33,8 +47,8 @@ public class PistonBlockEntityRendererMixin {
 	}
 	
 	private int getPistonDelay() {
-		int delay = pistonDelay.get();
-		int multiplier = delayMultiplier.get();
-		return multiplier * delay;
+		SettingsPack settings = sticky ? STICKY_PISTON : NORMAL_PISTON;
+		Setting<IntegerProperty> speedSetting = extending ? RISING_SPEED : FALLING_SPEED;
+		return GLOBAL.get(DELAY_MULTIPLIER) * settings.get(speedSetting);
 	}
 }
