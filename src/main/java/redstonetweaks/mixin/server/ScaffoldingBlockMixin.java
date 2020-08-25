8731ff604128fa9f6c94a20a9ccf5156109441d6
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 16:19:29 +0200

    Update ScaffoldingBlockMixin.java

--- src/main/java/redstonetweaks/mixin/server/ScaffoldingBlockMixin.java
+++ src/main/java/redstonetweaks/mixin/server/ScaffoldingBlockMixin.java
@@ -1,23 +1,25 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.scaffoldingDelay;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-import net.minecraft.block.ScaffoldingBlock;;
+import net.minecraft.block.ScaffoldingBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.TickScheduler;;
 
 @Mixin(ScaffoldingBlock.class)
 public class ScaffoldingBlockMixin {
 	
-	@ModifyConstant(method = "onBlockAdded", constant = @Constant(intValue = 1))
-	private int onBlockAddedGetDelay(int oldValue) {
-		return scaffoldingDelay.get();
+	@Redirect(method = "onBlockAdded", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onOnBlockAddedRedirectSchedule(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, SCAFFOLDING.get(DELAY), SCAFFOLDING.get(TICK_PRIORITY));
 	}
 	
-	@ModifyConstant(method = "getStateForNeighborUpdate", constant = @Constant(intValue = 1))
-	private int getStateForNeighborUpdateGetDelay(int oldValue) {
-		return scaffoldingDelay.get();
+	@Redirect(method = "getStateForNeighborUpdate", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/world/TickScheduler;schedule(Lnet/minecraft/util/math/BlockPos;Ljava/lang/Object;I)V"))
+	private <T> void onGetStateForNeighborUpdateRedirectSchedule(TickScheduler<T> tickScheduler, BlockPos pos, T object, int oldDelay) {
+		tickScheduler.schedule(pos, object, SCAFFOLDING.get(DELAY), SCAFFOLDING.get(TICK_PRIORITY));
 	}
 }
