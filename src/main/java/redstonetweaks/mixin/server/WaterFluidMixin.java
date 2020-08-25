eca766de35c33f1cdc76ccb042303a3d933b6b25
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2020-08-25 16:22:53 +0200

    Update WaterFluidMixin.java

--- src/main/java/redstonetweaks/mixin/server/WaterFluidMixin.java
+++ src/main/java/redstonetweaks/mixin/server/WaterFluidMixin.java
@@ -1,18 +1,25 @@
 package redstonetweaks.mixin.server;
 
-import static redstonetweaks.setting.Settings.waterDelay;
+import static redstonetweaks.setting.SettingsManager.*;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.fluid.WaterFluid;
+import net.minecraft.world.TickPriority;
+import redstonetweaks.helper.FluidHelper;
 
 @Mixin(WaterFluid.class)
-public class WaterFluidMixin {
-	
+public class WaterFluidMixin implements FluidHelper {
+
 	@ModifyConstant(method = "getTickRate", constant = @Constant(intValue = 5))
 	private int getWaterDelay(int oldDelay) {
-		return waterDelay.get();
+		return WATER.get(DELAY);
+	}
+	
+	@Override
+	public TickPriority getTickPriority() {
+		return WATER.get(TICK_PRIORITY);
 	}
 }
