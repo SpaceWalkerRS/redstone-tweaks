c99eff32e72a309b599f59fbbfbd54435605daeb
Space Walker <spacedoesrs@gmail.com>
2020-09-07 00:27:50 +0200

    Updated mod version to 0.8.0

--- src/main/java/redstonetweaks/RedstoneTweaks.java
+++ src/main/java/redstonetweaks/RedstoneTweaks.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 public class RedstoneTweaks implements ModInitializer {
 	
-	public static final RedstoneTweaksVersion MOD_VERSION = new RedstoneTweaksVersion(0, 7, 1);
+	public static final RedstoneTweaksVersion MOD_VERSION = new RedstoneTweaksVersion(0, 8, 0);
 	public static final RedstoneTweaksVersion SETTINGS_VERSION = new RedstoneTweaksVersion(1, 0, 0);
 	
 	private static RedstoneTweaks instance;
