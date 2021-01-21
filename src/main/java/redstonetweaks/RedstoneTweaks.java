80b66117a0c2fe0548403fe9393d0d870f628fe9
Space Walker <spacedoesrs@gmail.com>
2021-01-21 06:52:28 +0100

    Updated version to 0.9.0-pre3

--- src/main/java/redstonetweaks/RedstoneTweaks.java
+++ src/main/java/redstonetweaks/RedstoneTweaks.java
@@ -15,7 +15,7 @@ public class RedstoneTweaks implements ModInitializer {
 	
 	public static final Logger LOGGER = LogManager.getLogger("Redstone Tweaks");
 	
-	public static final RedstoneTweaksVersion MOD_VERSION = RedstoneTweaksVersion.createSnapshot(0, 9, 0, 2);
+	public static final RedstoneTweaksVersion MOD_VERSION = RedstoneTweaksVersion.createSnapshot(0, 9, 0, 3);
 	public static final RedstoneTweaksVersion PACKET_PROTOCOL = RedstoneTweaksVersion.createRelease(1, 0, 2);
 	public static final RedstoneTweaksVersion SETTINGS_VERSION = RedstoneTweaksVersion.createRelease(1, 2, 1);
 	
